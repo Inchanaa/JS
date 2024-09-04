7f58b5d605938d5bc6caeb04ef855fddb58f823f
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-09-04 17:06:19 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //to treat all js code as newer version
+let c=123n
+let a = null
+let b;
+console.log(a)
+console.log(b)
+console.log(c)
+
+// alert(3 + 3 ) -- v r using node nt browser
+/*
+number
+boolean
+bigint
+string
+null -->object--standalone value
+undefined
+symbols--> unique
+*/
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
