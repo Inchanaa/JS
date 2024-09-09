8f661cb35f0faf78ef303bcdd82d27447a1baef4
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-09-09 08:50:52 +0000

    comparison

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -1,20 +1,29 @@
-console.log(2 > 1);
-console.log("2" > 1);
-console.log("02" > 1);
+console.log(2 > 1);   //T
+console.log("2" > 1);   //T
+console.log("02" > 1);  //T
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0); //comparison converts null to a number treating it as 0
-console.log(null <= 0);
+console.log(null > 0);  //F
+console.log(null == 0); //F
+console.log(null >= 0); //TRUE-->comparison converts null to a number treating it as 0
+console.log(null <= 0);     //T
 
-console.log(undefined == 0);
-console.log(undefined >= 0);
-console.log(undefined < 0);
-console.log(undefined === 0);
+console.log(undefined == 0);    //F
+console.log(undefined >= 0);    //F
+console.log(undefined < 0);     //F
+console.log(undefined === 0);   //F
 
 // strict ====
 
-console.log("2" === 2);
+console.log("2" === 2);     //F
+console.log(null === 0);    //F
+console.log("null" === 0);  //F
+console.log(-0 === +0);   //T
+console.log(-2 === +2);  //fals
+console.log(NaN == NaN); // NaN is not equals to NaN
+
+
+
+
 
 
 
