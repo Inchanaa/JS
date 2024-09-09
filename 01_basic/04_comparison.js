56ecb813e123976600e048b802224047f37dd253
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-09-09 06:04:39 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+console.log(2 > 1);
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //comparison converts null to a number treating it as 0
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined === 0);
+
+// strict ====
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
